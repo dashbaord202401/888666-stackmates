d78f94ae479f400d7c0886020ecd14cf386bdae0
matt <matt@dreamineering.com>
2024-01-11 01:33:17 +0100

    rename stackmates directory

--- connection/apps/project/stackmates/jest.config.ts
+++ connection/apps/project/stackmates/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/apps/project/mates',
+  coverageDirectory: '../../../coverage/apps/project/stackmates',
 };
